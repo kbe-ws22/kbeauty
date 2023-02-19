9c375f511c948cca2ffef8afad37d7d278ee493f
Phuong Nguyen <nguyen_thi_phuong@live.de>
2023-02-19 23:07:14 +0100

    KBE-57 added homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import Slideshow from "../components/Slideshow.vue";
+import TextBlock from "../components/TextBlock.vue";
+
 </script>
 
 <template>
   <main>
     <Slideshow />
+    <TextBlock />
   </main>
 </template>
